2de115f6d1430d33c153978145a597a1cb815320
Edmund Chege <edmundshege@gmail.com>
2021-02-09 20:00:26 +0000

    funcs and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - add two integers
+ * @x: 1st argument
+ * @y: 2nd argument
+ * Return: sum
+ */
+int add(int a, int b)
+{
+  int sum;
+  
+  sum = a + b;
+  return(sum);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * abs - computes absolute value of an integer.
+ *
+ * Return: 0.
+ */
+
+int _abs(int)
+{
+  if (n > 0)
+    {
+      return (n);
+    }
+  else if (n == 0)
+    {
+      return (0);
+    }
+  else
+    {
+      return (-n);
+    }
+  return 0;
+}
