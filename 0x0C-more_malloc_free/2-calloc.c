c70637ec56b7bb599859e90bce7ff91d0487a10f
Edmund Chege <edmundshege@gmail.com>
2021-03-08 20:32:01 +0000

    changes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,17 +11,21 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
   void *ptr;
-  unsigned int i;
+  unsigned int i = 0;
 
-  if (nmemb <= 0 || size <= 0)
+  if (nmemb == 0 || size == 0)
     return (NULL);
 
   ptr = malloc(nmemb * size);
+  
   if (ptr == NULL)
     return (NULL);
 
-  for (i = 0; i < nmemb * size; i++)
-    *((char *)ptr + i) = 0;
+  while (i < 0 (nmemb * size))
+    {
+      *((char *)ptr + i) = 0;
+      i++;
+    }
 
   return (ptr);
 }
