055ecebc22be2f2c513d6a253b2bd89d51b4ae31
Edmund Chege <edmundshege@gmail.com>
2021-03-08 19:54:18 +0000

    preprocessor

---
+++ 0x0D-preprocessor/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+/**
+ * main - prints current filename to std output
+ * Return: 0 on success
+ */
+
+int main(void)
+
+#endif /* HOLBERTON_H */
